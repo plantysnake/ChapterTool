c71bc4ea942500c9dafab7fd81ef816d4a5f174a
tautcony <tautcony@gmail.com>
2015-09-14 17:38:19 +0800

    import some source code from MeGUI

---
+++ Time_Shift/Util/ifoData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace ChapterTool.Util
+{
+    class ifoData
+    {
+        private byte[] data;
+        //public List<Clip> chapterClips;
+
+        public ifoData(string path)
+        {
+            data = File.ReadAllBytes(path);
+            //chapterClips = new List<Clip>();
+        }
+    }
+}
