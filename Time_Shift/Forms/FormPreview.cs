b2513fd98ef423fea7f9c6575f6dbbf8fd375074
tautcony <tautcony@gmail.com>
2015-12-20 15:25:16 +0800

    show horizon scorll bar automatically

--- Time_Shift/Forms/FormPreview.cs
+++ Time_Shift/Forms/FormPreview.cs
@@ -15,29 +15,35 @@ namespace ChapterTool.Forms
             InitializeComponent();
             cTextBox1.Text = text;
             _mainPos       = pos;
-            if (cTextBox1.Lines.Length>20)
+            ScrollBarSet();
+            Icon = Icon.ExtractAssociatedIcon(Assembly.GetExecutingAssembly().Location);
+        }
+
+        private void ScrollBarSet()
+        {
+            if (cTextBox1.Lines.Select(item => item.Length).Max() > 40 && cTextBox1.Lines.Length > 20)
+            {
+                cTextBox1.ScrollBars = ScrollBars.Both;
+            }
+            else if (cTextBox1.Lines.Length > 20)
             {
                 cTextBox1.ScrollBars = ScrollBars.Vertical;
             }
-            Icon = Icon.ExtractAssociatedIcon(Assembly.GetExecutingAssembly().Location);
         }
 
         public void UpdateText(string text)
         {
             cTextBox1.Text = text;
+            ScrollBarSet();
         }
 
-        private void cTextBox1_DoubleClick(object sender, EventArgs e)
-        {
-            Close();
-        }
-
+        private void cTextBox1_DoubleClick(object sender, EventArgs e) => Close();
 
 
         private void FormPreview_Load(object sender, EventArgs e)
         {
-            _mainPos.X = _mainPos.X - 230;
-            Location = _mainPos;
+            _mainPos.X = _mainPos.X - Size.Width;
+            Location   = _mainPos;
         }
 
         private void FormPreview_FormClosing(object sender, FormClosingEventArgs e)
