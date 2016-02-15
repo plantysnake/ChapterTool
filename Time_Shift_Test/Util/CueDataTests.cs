d64411d91f20d1800ff1421ae0f3b79e19f606f7
tautcony <tautcony@gmail.com>
2016-02-15 16:25:15 +0800

    New Unit test.

---
+++ Time_Shift_Test/Util/CueDataTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ChapterTool.Util;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChapterTool.Util.Tests
+{
+    [TestClass()]
+    public class CueDataTests
+    {
+        [TestMethod()]
+        public void PraseCueTest()
+        {
+            const string path = @"..\..\[cue_Sample]\ARCHIVES 2.cue";
+            var result = CueData.PraseCue(File.ReadAllText(path));
+            Assert.IsTrue(result.Chapters.Count == 4);
+        }
+    }
+}
