26850aa5af8f3e9539ea1f1aca03495005f17cff
tautcony <tautcony@gmail.com>
2016-03-03 11:14:28 +0800

    Improve XplData's function again.

--- Time_Shift/Util/XplData.cs
+++ Time_Shift/Util/XplData.cs
@@ -14,8 +14,8 @@ namespace ChapterTool.Util
             XNamespace ns = "http://www.dvdforum.org/2005/HDDVDVideo/Playlist";
             foreach (XElement ts in doc.Element(ns + "Playlist").Elements(ns + "TitleSet"))
             {
-                float timeBase = GetFps((string)ts.Attribute("timeBase")) ?? 60;
-                float tickBase = GetFps((string)ts.Attribute("tickBase")) ?? 60;
+                var timeBase = GetFps((string)ts.Attribute("timeBase")) ?? 60; //required
+                var tickBase = GetFps((string)ts.Attribute("tickBase")) ?? 60; //optional
                 foreach (XElement title in ts.Elements(ns + "Title").Where(t => t.Element(ns + "ChapterList") != null))
                 {
                     ChapterInfo pgc = new ChapterInfo
@@ -26,44 +26,58 @@ namespace ChapterTool.Util
                         Chapters        = new List<Chapter>()
                     };
 
-                    int tickBaseDivisor = (int?)title.Attribute("tickBaseDivisor") ?? 1;
+                    int tickBaseDivisor = (int?)title.Attribute("tickBaseDivisor") ?? 1; //optional
                     pgc.Duration        = GetTimeSpan((string)title.Attribute("titleDuration"), timeBase, tickBase, tickBaseDivisor);
-                    string titleName    = Path.GetFileNameWithoutExtension(location);
-                    if (title.Attribute("id") != null)          titleName = (string)title.Attribute("id");
-                    if (title.Attribute("displayName") != null) titleName = (string)title.Attribute("displayName");
-                    pgc.Title = titleName;
+                    var titleName       = Path.GetFileNameWithoutExtension(location);
+                    if (title.Attribute("id") != null)          titleName = title.Attribute("id").Value; //optional
+                    if (title.Attribute("displayName") != null) titleName = title.Attribute("displayName").Value; //optional
+                    pgc.Title           = titleName;
                     foreach (XElement chapter in title.Element(ns + "ChapterList").Elements(ns + "Chapter"))
                     {
+                        var chapterName = string.Empty;
+                        if (chapter.Attribute("id") != null) chapterName = chapter.Attribute("id").Value; //optional
+                        if (chapter.Attribute("displayName") != null) chapterName = chapter.Attribute("displayName").Value; //optional
                         pgc.Chapters.Add(new Chapter
                         {
-                            Name = (string)chapter.Attribute("displayName"),
-                            Time = GetTimeSpan((string)chapter.Attribute("titleTimeBegin"), timeBase, tickBase, tickBaseDivisor)
+                            Name = chapterName,
+                            Time = GetTimeSpan((string)chapter.Attribute("titleTimeBegin"), timeBase, tickBase, tickBaseDivisor) //required
                         });
                     }
-                    //pgc.ChangeFps(24D / 1.001D);
                     yield return pgc;
                 }
             }
         }
 
-        private static float? GetFps(string fps)
+        /// <summary>
+        /// Eg: Convert string "\d+fps" to a double value
+        /// </summary>
+        /// <param name="fps"></param>
+        /// <returns></returns>
+        private static double? GetFps(string fps)
         {
             if (string.IsNullOrEmpty(fps)) return null;
             fps = fps.Replace("fps", string.Empty);
             return float.Parse(fps);
         }
 
-        private static TimeSpan GetTimeSpan(string timeSpan, float timeBase, float tickBase, int tickBaseDivisor)
+        /// <summary>
+        /// Constructs a TimeSpan from a string formated as "HH:MM:SS:TT"
+        /// </summary>
+        /// <param name="timeSpan"></param>
+        /// <param name="timeBase"></param>
+        /// <param name="tickBase"></param>
+        /// <param name="tickBaseDivisor"></param>
+        /// <returns></returns>
+        private static TimeSpan GetTimeSpan(string timeSpan, double timeBase, double tickBase, int tickBaseDivisor)
         {
-            TimeSpan ts = TimeSpan.Parse(timeSpan.Substring(0, timeSpan.LastIndexOf(':')));
-            ts = new TimeSpan((long)((ts.TotalSeconds / 60D) * timeBase) * TimeSpan.TicksPerSecond);
+            var colonPosition = timeSpan.LastIndexOf(':');
+            TimeSpan ts = TimeSpan.Parse(timeSpan.Substring(0, colonPosition));
+            ts = new TimeSpan((long)(ts.TotalSeconds / 60D * timeBase) * TimeSpan.TicksPerSecond);
 
             //convert ticks to ticks timebase
-            //NOTE: eac3to does not utilize tickBaseDivisor (they have bug)
-            decimal convert = TimeSpan.TicksPerSecond / ((decimal)tickBase / tickBaseDivisor);
-            decimal ticks = decimal.Parse(timeSpan.Substring(timeSpan.LastIndexOf(':') + 1)) * convert;
+            decimal newTick = TimeSpan.TicksPerSecond / ((decimal)tickBase / tickBaseDivisor);
+            decimal ticks   = decimal.Parse(timeSpan.Substring(colonPosition + 1)) * newTick;
             return ts.Add(new TimeSpan((long)ticks));
         }
-
     }
 }
