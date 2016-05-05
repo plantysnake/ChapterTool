25d2db1b07f8805068552ab54c3b57e8cdc770e7
tautcony <tautcony@gmail.com>
2016-05-05 09:26:57 +0800

    Update unit test.

--- Time_Shift_Test/Knuckleball/MP4FileTests.cs
+++ Time_Shift_Test/Knuckleball/MP4FileTests.cs
@@ -1,12 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Knuckleball;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using ChapterTool.Util;
+using System;
 
 namespace Knuckleball.Tests
 {
@@ -14,20 +9,29 @@ namespace Knuckleball.Tests
     public class MP4FileTests
     {
         [TestMethod()]
-        public void OpenTest()
+        public void Mp4ChapterTest()
         {
             const string path = @"..\..\..\[Video_Sample]\Chapter.mp4";
-            Knuckleball.MP4File file = MP4File.Open(path);
-            foreach (var chapter in file.Chapters)
-            {
-                Debug.WriteLine(chapter);
-            }
 
-            Mp4Data data = new Mp4Data(path);
-            var t = data.Chapter;
-            foreach (var chapter in t.Chapters)
+            var expectResult = new[]
+            {
+                new { Name = "Chapter 01", Time = "00:00:00.000" },
+                new { Name = "Chapter 02", Time = "00:00:10.000" },
+                new { Name = "Chapter 03", Time = "00:00:20.000" },
+                new { Name = "Chapter 04", Time = "00:00:30.000" },
+            };
+
+            Mp4Data result = new Mp4Data(path);
+
+            Assert.IsTrue(result.Chapter.Chapters.Count == 4);
+
+            int index = 0;
+            foreach (var chapter in result.Chapter.Chapters)
             {
                 Console.WriteLine(chapter);
+                Debug.Assert(expectResult[index].Name == chapter.Name);
+                Debug.Assert(expectResult[index].Time == chapter.Time.Time2String());
+                ++index;
             }
         }
     }
--- Time_Shift_Test/Util/CueDataTests.cs
+++ Time_Shift_Test/Util/CueDataTests.cs
@@ -1,11 +1,7 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using ChapterTool.Util;
-using System;
-using System.Collections.Generic;
+﻿using System;
+using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ChapterTool.Util.Tests
 {
@@ -16,8 +12,27 @@ namespace ChapterTool.Util.Tests
         public void PraseCueTest()
         {
             const string path = @"..\..\..\[cue_Sample]\ARCHIVES 2.cue";
+
+            var expectResult = new[]
+            {
+                new { Name = "オーディオドラマ・1stパート", Time = "00:00:00.000"},
+                new { Name = "初色bloomy [初春飾利(豊崎愛生)]", Time = "00:15:19.280"},
+                new { Name = "オーディオドラマ・2ndパート", Time = "00:19:15.093"},
+                new { Name = "ナミダ御免のGirls Beat [佐天涙子(伊藤かな恵)]", Time = "00:32:12.173"},
+            };
+
             var result = CueData.PraseCue(File.ReadAllText(path));
+
             Assert.IsTrue(result.Chapters.Count == 4);
+
+            int index = 0;
+            foreach (var chapter in result.Chapters)
+            {
+                Console.WriteLine(chapter.ToString());
+                Debug.Assert(expectResult[index].Name == chapter.Name);
+                Debug.Assert(expectResult[index].Time == chapter.Time.Time2String());
+                ++index;
+            }
         }
     }
 }
