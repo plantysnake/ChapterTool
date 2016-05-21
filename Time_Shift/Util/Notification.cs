b9e87a586d012caf2be6606e3cc89c113192c4e4
tautcony <tautcony@gmail.com>
2016-05-21 23:30:22 +0800

    Show stack trace in DEBUG.

--- Time_Shift/Util/Notification.cs
+++ Time_Shift/Util/Notification.cs
@@ -9,8 +9,11 @@ namespace ChapterTool.Util
         public static DialogResult ShowError(string argMessage, Exception exception)
         {
             return MessageBox.Show(caption: Resources.Message_ChapterTool_Error,
-                text: $"{argMessage}:{Environment.NewLine}{exception.Message}",
-                buttons: MessageBoxButtons.OK, icon: MessageBoxIcon.Hand);
+                text: $"{argMessage}:{Environment.NewLine}{exception.Message}"
+#if DEBUG
+                + $"{Environment.NewLine}{exception.StackTrace}"
+#endif
+                , buttons: MessageBoxButtons.OK, icon: MessageBoxIcon.Hand);
         }
 
         public static DialogResult ShowInfo(string argMessage)
