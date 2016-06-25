0fe501f661a848b10166bab79eb6f2f6cf5b821f
tautcony <tautcony@gmail.com>
2016-06-26 01:10:56 +0800

    No more RegStorage.

--- Time_Shift/Util/convertMethod.cs
+++ Time_Shift/Util/convertMethod.cs
@@ -27,6 +27,7 @@ using System.Diagnostics;
 using System.Security.Principal;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
+using Microsoft.Win32;
 
 namespace ChapterTool.Util
 {
@@ -182,6 +183,58 @@ namespace ChapterTool.Util
                 //Do nothing. Probably the user canceled the UAC window
             }
             return false;
+        }
+    }
+
+    public static class RegistryStorage
+    {
+        public static string Load(string subKey = @"Software\ChapterTool", string name = "SavingPath")
+        {
+            string path = string.Empty;
+            // HKCU_CURRENT_USER\Software\
+            var registryKey = Registry.CurrentUser.OpenSubKey(subKey);
+            if (registryKey == null) return path;
+            path = (string)registryKey.GetValue(name);
+            registryKey.Close();
+            return path;
+        }
+
+        public static void Save(string value, string subKey = @"Software\ChapterTool", string name = "SavingPath")
+        {
+            // HKCU_CURRENT_USER\Software\
+            var registryKey = Registry.CurrentUser.CreateSubKey(subKey);
+            registryKey?.SetValue(name, value);
+            registryKey?.Close();
+        }
+
+        /// <summary>
+        /// 创建文件关联
+        /// </summary>
+        /// <param name="programFile">应用程序文件的完整路径("C:\abc\def.exe")</param>
+        /// <param name="extension">文件扩展名（例如 ".txt"）</param>
+        /// <param name="typeName">文件类型名称</param>
+        /// <param name="project">指向的文件打开方式</param>
+        /// <param name="argument">附加参数（不包括"%1"）</param>
+        public static void SetOpenMethod(string programFile, string extension, string typeName, string project, string argument = "")
+        {
+            Registry.ClassesRoot.CreateSubKey(extension)?.SetValue(typeName, project, RegistryValueKind.String);
+
+            RegistryKey subKey = Registry.ClassesRoot.CreateSubKey(project);
+            subKey = subKey?.CreateSubKey("shell");
+            subKey = subKey?.CreateSubKey("open");
+            subKey = subKey?.CreateSubKey("command");
+            subKey?.SetValue("", $@"""{programFile}"" ""%1"" {argument}", RegistryValueKind.ExpandString);
+            subKey?.Dispose();
+            NativeMethods.RefreshNotify();
+        }
+
+        public static int RegistryAddCount(string subKey, string name, int delta = 1)
+        {
+            var countS = Load(subKey, name);
+            int count = string.IsNullOrEmpty(countS) ? 0 : int.Parse(countS);
+            count += delta;
+            Save(count.ToString(), subKey, name);
+            return count - delta;
         }
     }
 }
