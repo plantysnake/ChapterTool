3d047ea605b089e3adcf9dceb9a2e4ef1ac1f2ce
tautcony <tautcony@gmail.com>
2016-08-04 22:32:00 +0800

    Update license.

--- Time_Shift/Util/Clip.cs
+++ Time_Shift/Util/Clip.cs
@@ -1,6 +1,6 @@
 // ****************************************************************************
 //
-// Copyright (C) 2014-2015 TautCony (TautCony@vcb-s.com)
+// Copyright (C) 2014-2016 TautCony (TautCony@vcb-s.com)
 //
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
