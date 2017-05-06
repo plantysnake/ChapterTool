061959153e55ffe2feef5530dcd0323c78e0f748
tautcony <tautcony@gmail.com>
2017-05-06 10:42:39 +0800

    update copyright time info.

--- Time_Shift/Util/ChapterData/MplsData.cs
+++ Time_Shift/Util/ChapterData/MplsData.cs
@@ -1,6 +1,6 @@
 ﻿// ****************************************************************************
 //
-// Copyright (C) 2014-2016 TautCony (TautCony@vcb-s.com)
+// Copyright (C) 2014-2017 TautCony (TautCony@vcb-s.com)
 //
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
